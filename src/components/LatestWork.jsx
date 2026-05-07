a8ceefb549169f4eea89ae1a05691bdd2a5db069
mzain030 <mzainnaseer786@gmail.com>
2026-05-07 20:50:00 +0500

    create app and also create file name zain work and lates work

---
+++ src/components/LatestWork.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LatestWork = () => {
+  return (
+    <div>
+      <h1>Latest Work</h1>
+      This is the latest work component. It will display the most recent projects and updates related to my work. Stay tuned for more information and updates on my latest endeavors in the field of technology and development.
+    </div>
+  )
+}
+
+export default LatestWork
---
+++ src/components/ZainWork.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ZainWork = () => {
+  return (
+    <div>
+      <h1>Zain Work</h1>
+    </div>
+  )
+}
+
+export default ZainWork
